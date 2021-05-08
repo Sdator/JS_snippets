c1d24bd39af1b005112455ced170692915967f42
Sdator <250740270@qq.com>
2021-05-09 05:51:36 +0800

    +

--- 代码片段/南+任务.js
+++ 代码片段/南+任务.js
@@ -1,38 +1,77 @@
 import fetch from "node-fetch";
+import HttpsProxyAgent from "https-proxy-agent";
+import WEB from "../lib/WEB.js";
 
-async function 任务请求(任务id, 操作类型) {
-  const data = {
-    H_name: "tasks",
-    action: "ajax",
-    cid: 任务id,
-    actions: 操作类型,
-    nowtime: new Date().getTime(),
-    verify: "e419085d",
-  };
-  const url = new URL("https://www.south-plus.net/plugin.php");
-  // 构建url
-  for (const v of Object.entries(data)) {
-    url.searchParams.append(...v);
-  }
-  const res = await fetch(url);
-  console.log(res);
-  console.log(res);
-
-  return await res.text();
-}
-(async () => {
-  const 操作类型 = {
+// 立即执行类
+class Nadd extends WEB {
+  static #操作类型 = {
     申请任务: "job",
     完成任务: "job2",
   };
-  const 任务ID列表 = [14, 15];
-  for (const ID of 任务ID列表) {
-    let msg = await 任务请求(ID, 操作类型.申请任务);
-    if (msg.includes("已经申请")) console.log("申请成功");
-    else console.log("已经申请过了");
+  static #任务ID列表 = [14, 15]; // 14 周常   15 日常
 
-    msg = await 任务请求(ID, 操作类型.完成任务);
+  constructor() {
+    super();
+    // 默认执行 每日签到
+    Nadd.init();
+  }
+
+  static init() {
+    this.签到(this.#任务ID列表[0]);
+  }
+
+  static async 签到(任务id) {
+    let msg = await this.任务(任务id, this.#操作类型.申请任务);
+
+    console.log(msg);
+
+    if (msg.includes("已经申请")) console.log("申请成功");
+    else throw new Error("已经申请过了或申请失败");
+
+    msg = await this.任务(任务id, this.#操作类型.完成任务);
     if (msg.includes("完成")) console.log("任务完成");
     else console.log("任务失败");
   }
-})();
+
+  // plugin.php?H_name=tasks&action=ajax&actions=job&cid='+id,
+  static async 任务(任务id, 操作类型) {
+    const data = {
+      H_name: "tasks",
+      action: "ajax",
+      cid: 任务id,
+      actions: 操作类型,
+      nowtime: new Date().getTime(),
+      // verify: "e419085d",
+    };
+    const url = new URL("https://www.south-plus.net/plugin.php");
+    // 构建url
+    for (const v of Object.entries(data)) {
+      url.searchParams.append(...v);
+    }
+    const res = await fetch(url, {
+      // agent: new HttpsProxyAgent("http://" + this.ip + ":" + this.port), // 使用代理
+      headers: {
+        Cookie: "",
+      },
+    });
+
+    return await res.text();
+  }
+}
+
+// (async () => {
+//   const 操作类型 = {
+//     申请任务: "job",
+//     完成任务: "job2",
+//   };
+//   const 任务ID列表 = [14, 15];
+//   for (const ID of 任务ID列表) {
+//     let msg = await 任务(ID, 操作类型.申请任务);
+//     if (msg.includes("已经申请")) console.log("申请成功");
+//     else console.log("已经申请过了");
+
+//     msg = await 任务(ID, 操作类型.完成任务);
+//     if (msg.includes("完成")) console.log("任务完成");
+//     else console.log("任务失败");
+//   }
+// })();
