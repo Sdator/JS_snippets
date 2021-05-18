92060487aeb510000f30a8d82086f2c270a4758e
Sdator <250740270@qq.com>
2021-05-19 05:06:20 +0800

    完成chrome驱动自动更新

--- 代码片段/南+任务.js
+++ 代码片段/南+任务.js
@@ -1,15 +1,28 @@
-const fetch = require("node-fetch");
-const HttpsProxyAgent = require("https-proxy-agent");
-const WEB = require("../lib/WEB.js");
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
-// const { exec } = require("child_process");
-const iconv = require("iconv-lite");
+// const fetch = require("node-fetch");
+// const HttpsProxyAgent = require("https-proxy-agent");
+// const WEB = require("../lib/WEB.js");
+// const util = require("util");
+// const exec = util.promisify(require("child_process").exec);
+// const iconv = require("iconv-lite");
+// const request = require("request");
+// const express = require("express");
+// const fs = require("fs");
+// const app = express();
 
-// import fetch from "node-fetch";
-// import HttpsProxyAgent from "https-proxy-agent";
-// import WEB from "../lib/WEB.js";
-// import { exec } from "child_process";
+import fetch from "node-fetch";
+import HttpsProxyAgent from "https-proxy-agent";
+import WEB from "../lib/WEB.js";
+import child_process from "child_process";
+import util from "util";
+import iconv from "iconv-lite";
+import AdmZip from "adm-zip";
+
+const exec = util.promisify(child_process.exec);
+import request from "request";
+import express from "express";
+import fs from "fs";
+const app = express();
+// import AdmZip from "adm-zip-iconv";
 
 // 立即执行类
 class Nadd extends WEB {
@@ -70,18 +83,51 @@ class Nadd extends WEB {
 }
 
 // console.log(navigator.userAgent);
+/**
+ *
+ * @param {String} ver 版本号
+ * @returns
+ */
+async function 获取下载地址(ver) {
+  // 获取对应版本的驱动程序下载地址
+  const res = await fetch("http://npm.taobao.org/mirrors/chromedriver/");
+  const data = await res.text();
+  // 动态组建正则 匹配出
+  const r = new RegExp(`<a href="(.*${ver}.*)">`);
+  const path = r.exec(data)[1];
+  const filename = "chromedriver_win32.zip";
+  return "http://npm.taobao.org" + path + filename;
+}
 
 async function 获取chrome版本() {
+  // !过滤出版本号
   const com = String.raw`wmic datafile where name="C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe" get Version /value`;
   // 使用 { encoding: "buffer" } 解决乱码问题
   const { stdout, stderr } = await exec(com, { encoding: "buffer" });
-
+  return /\d+.\d+.\d+/.exec(stdout).toString();
   // console.log(iconv.decode(stdout, "cp936"));
-
-  // console.log(stderr);
 }
 
-获取chrome版本();
+async function 下载驱动() {
+  const ver = await 获取chrome版本();
+  const url = await 获取下载地址(ver);
+  // ! 下载驱动压缩包
+  const filename = "chromedriver_win32.zip";
+  request(url)
+    .pipe(fs.createWriteStream(filename))
+    .on("close", function (err) {
+      console.log("下载完毕");
+      // ! 解压文件
+      const unzip = new AdmZip(filename);
+      // 解压并覆盖文件
+      unzip.extractAllTo("./bin", true);
+      // 解压压缩包中某个文件
+      // file.extractEntryTo("hello.txt", "./");
+      console.log("解压完成");
+    });
+}
+
+下载驱动();
 
 // console.log(exec("dir"));
 
